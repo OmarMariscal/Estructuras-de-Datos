aa5ff1ee9d42b1cf861fdf5f03d7fddcf32934af
OmarMariscal <omarmariscal543@gmail.com>
2025-11-05 22:36:35 -0600

    Tarea 12. Código y Documentación.

---
+++ Tarea13/include/configure.hpp
@@ -0,0 +1,11 @@
+#ifndef __CONFIGURE_H__
+#define __CONFIGURE_H__
+
+namespace Configure {
+static const int predeterminatedSizeWindows =
+    60;  // Controla el tamaño de los títulos y lo centrado de los textos de
+         // forma Global
+static const int minimunRandNum = 0;
+static const int maximunRandNum = 100000;
+}  // namespace Configure
+#endif  // __CONFIGURE_H__
---
+++ Tarea13/include/ownexceptions.hpp
@@ -0,0 +1,62 @@
+#ifndef __OWNEXCEPTIONS_H__
+#define __OWNEXCEPTIONS_H__
+
+#include <stdexcept>
+#include <string>
+
+namespace DataContainersExceptions {
+class MemoryDeficiency : public std::runtime_error {
+ public:
+  explicit MemoryDeficiency(const std::string& msg = "Insuficiencia de Memoria")
+      : std::runtime_error(msg) {}
+};
+
+
+class InvalidPosition : public std::runtime_error {
+ public:
+  explicit InvalidPosition(
+      const std::string& msg = "La posicion Ingresada es Invalida")
+      : std::runtime_error(msg) {}
+};
+}  // namespace DataContainersExceptions
+
+namespace InputExceptions {
+class InvalidOption : public std::runtime_error {
+ public:
+  explicit InvalidOption(
+      const std::string& msg = "La opcion ingresada esta fuera de rango")
+      : runtime_error(msg) {}
+};
+
+class EmptyString : public std::runtime_error {
+ public:
+  explicit EmptyString(
+      const std::string& msg = "El string no puede estar vacio")
+      : runtime_error(msg) {}
+};
+
+class OperationCanceledException : public std::runtime_error {
+ public:
+  explicit OperationCanceledException(
+      const std::string msg = "Operacion Cancelada")
+      : runtime_error(msg) {}
+};
+
+class NumberNotPositive : public std::runtime_error {
+ public:
+  explicit NumberNotPositive(
+      const std::string& msg = "Esta entrada debe ser mayor a 0")
+      : runtime_error(msg) {}
+};
+}  // namespace InputExceptions
+
+
+namespace NodeExceptions {
+class NodeExceptionTo : std::runtime_error {
+ public:
+  explicit NodeExceptionTo(const std::string& msg = "Ha ocurrido un error al asignar la memoria")
+      : runtime_error(msg) {}
+};
+}  // namespace NodeExceptions
+
+#endif  // __OWNEXCEPTIONS_H__
